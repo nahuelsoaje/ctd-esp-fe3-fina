81dc98aa1dfad18c528172b677ab17d32da5a081
nahuelsoaje <soajenahuel9@gmail.com>
2024-04-23 18:54:49 -0300

    arreglando boton de modo oscuro

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
       
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <div className='dark'>
-      <button onClick={handleTheme}>Change theme</button>
+      <button onClick={handleTheme}>🌙</button>
       </div>
       
     </nav>
